3d9891b0fc09219b27dacf36f9319e2307346a19
zhangyumengzhang <17301115@bjtu.edu.cn>
2021-01-24 23:50:56 +0800

    导航栏加整体框架

---
+++ src/views/rnn.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+   <el-upload
+  class="upload-demo"
+  drag
+  action="https://jsonplaceholder.typicode.com/posts/"
+  multiple>
+  <i class="el-icon-upload"></i>
+  <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+  <div class="el-upload__tip" slot="tip">只能上传wav/mp3格式文件</div>
+</el-upload>
+</div>
+</template>
